4b60c79e8df0c42c9140bdfdf01d0b758ba1fe24
tiancong <359193813@qq.com>
2023-06-21 16:46:41 +0800

    feat: 暂存

--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 
 # 发版
 
+### 保证版本清爽, 建议使用 beta 更新
+
 - 更改 package.json
   `npm version prerelease --preid=beta`
+  - 0.0.1-beta.1->0.0.1-beta.2 git 打上 tag 并提交
 - 发布 beta 版
   ` npm publish --tag=beta`
+- npm version patch
+- npm version minor
+- npm version major
--- token.config.ts
+++ token.config.ts
@@ -7,7 +7,10 @@ interface TokenItem {
 }
 const tokensConfig: Record<string, TokenItem> = {
   sprint63: {
-    token: [],
+    token: [
+      "6832361f8b4ace5567b33763e942a85f50df621a7880c1e7ce01170fafbc8750",
+      "82e947061977ee9f69ba0d4e852e50a57b34c92efa60a528037521695eaf189c",
+    ],
     cateIds: [],
   },
   sprint64: {
