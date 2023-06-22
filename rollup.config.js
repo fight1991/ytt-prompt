f558bea7e033aab494d7b18bb168d055f1c62327
tiancong <359193813@qq.com>
2023-06-22 21:28:25 +0800

    feat: 大版本更新

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,6 @@ import typescript from "rollup-plugin-typescript2";
 import glob from "glob";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import resolve from "@rollup/plugin-node-resolve";
 
 export default [
   {
@@ -16,14 +15,6 @@ export default [
     },
     plugins: [json()],
   },
-  // {
-  //   input: "src/tools/index.ts",
-  //   output: {
-  //     file: "dist/es/index.js",
-  //     format: "esm",
-  //   },
-  //   plugins: [typescript()],
-  // },
   {
     input: "./src/index.ts",
     output: {
@@ -32,24 +23,24 @@ export default [
       assetFileNames: "[name][extname]",
       preserveModules: true,
     },
-    plugins: [typescript()],
+    plugins: [typescript({ exclude: ["./src/core/*"] })],
   },
-  // {
-  //   input: Object.fromEntries(
-  //     glob
-  //       .sync("src/ytt/**/*.ts")
-  //       .map((file) => [
-  //         path.relative(
-  //           "src",
-  //           file.slice(0, file.length - path.extname(file).length)
-  //         ),
-  //         fileURLToPath(new URL(file, import.meta.url)),
-  //       ])
-  //   ),
-  //   output: {
-  //     format: "esm",
-  //     dir: "dist/apis",
-  //   },
-  //   plugins: [typescript({ exclude: ["./src/tools/*"] })], // 多个入口, 每次执行都会读取tsconfig.json配置, 所以排除tools文件
-  // },
+  {
+    input: Object.fromEntries(
+      glob
+        .sync("src/core/**/*.ts")
+        .map((file) => [
+          path.relative(
+            "src",
+            file.slice(0, file.length - path.extname(file).length)
+          ),
+          fileURLToPath(new URL(file, import.meta.url)),
+        ])
+    ),
+    output: {
+      format: "cjs",
+      dir: "dist/cjs",
+    },
+    plugins: [typescript({ exclude: ["./src/tools/*", "./src/ytt/**/*.ts"] })], // 多个入口, 每次执行都会读取tsconfig.json配置, 所以排除tools文件
+  },
 ];
